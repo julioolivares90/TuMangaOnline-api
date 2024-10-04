e5c690e39f9d170dd2f3863f397cdfd3f2bc98f5
Julio Olivares <julioolivares90@hotmail.com>
2024-10-03 19:46:12 -0600

    se agrega uso de chromedp para obtener los mangas

--- core/tumangaonline/TuMangaOnlineScraper.go
+++ core/tumangaonline/TuMangaOnlineScraper.go
@@ -5,12 +5,18 @@ import (
 
 	s "strings"
 
+	"log"
+
+	"context"
+
+	"github.com/chromedp/cdproto/cdp"
+	"github.com/chromedp/chromedp"
 	"github.com/gocolly/colly/v2"
 	"github.com/julioolivares90/TumangaOnlineApi/models"
 	"github.com/julioolivares90/TumangaOnlineApi/utilities"
 )
 
-//GetMangasPopulares obtiene los mangas mas populares
+// GetMangasPopulares obtiene los mangas mas populares
 func GetMangasPopulares(pageNumber int) []models.MangaTMO {
 	var mangasPopulares []models.MangaTMO
 	url := utilities.TUMANGAONLINE_BASE_URL
@@ -32,11 +38,15 @@ func GetMangasPopulares(pageNumber int) []models.MangaTMO {
 
 		})
 	})
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL)
+	})
 	c.Visit(fmt.Sprintf("%s/populars?page=%d", url, pageNumber))
 	return mangasPopulares
 }
 
-//GetMangasPopularesJosei
+// GetMangasPopularesJosei
 func GetMangasPopularesJosei() []models.MangaTMO {
 	var mangasPopulares []models.MangaTMO
 	url := utilities.TUMANGAONLINE_BASE_URL
@@ -58,37 +68,87 @@ func GetMangasPopularesJosei() []models.MangaTMO {
 
 		})
 	})
-	c.Visit(fmt.Sprintf("%s/populars-girls", url))
+	c.Visit(fmt.Sprintf("%s/", url))
 	return mangasPopulares
 }
 
-//GetMangasPopularesSeinen
+// GetMangasPopularesSeinen
 func GetMangasPopularesSeinen() []models.MangaTMO {
 	var mangasPopulares []models.MangaTMO
 	url := utilities.TUMANGAONLINE_BASE_URL
-	c := colly.NewCollector()
 
-	c.OnHTML("#app > main > div:nth-child(2) > div.col-12.col-lg-8.col-xl-9 > div:nth-child(1)", func(element *colly.HTMLElement) {
-		element.ForEach("div.element", func(i int, element *colly.HTMLElement) {
-			dataItentificador := element.Attr("data-identifier")
-			//fmt.Println(dataItentificador)
-			mangaPopular := models.MangaTMO{
-				Title:       element.ChildText("a > div > div > h4"),
-				MangaUrl:    element.ChildAttr("a", "href"),
-				Type:        element.ChildText("a > div > span.book-type"),
-				Demography:  element.ChildText("a > div > span.demography"),
-				Score:       element.ChildText("a > div > span.score > span"),
-				MangaImagen: getImagenManga(element.ChildText("a > div > style"), dataItentificador),
-			}
-			mangasPopulares = append(mangasPopulares, mangaPopular)
+	// initializing a chrome instance
+	ctx, cancel := chromedp.NewContext(
+		context.Background(),
+		chromedp.WithLogf(log.Printf),
+	)
+	defer cancel()
 
+	// navigate to the target web page and select the HTML elements of interest
+	var nodes []*cdp.Node
+	chromedp.Run(ctx,
+		chromedp.Navigate(url),
+		chromedp.Nodes("body", &nodes, chromedp.ByQueryAll),
+	)
+
+	var url2, image, name, price string
+	for _, node := range nodes {
+
+		chromedp.Run(ctx,
+			chromedp.AttributeValue("a", "href", &url2, nil, chromedp.ByQuery, chromedp.FromNode(node)),
+			chromedp.AttributeValue("img", "src", &image, nil, chromedp.ByQuery, chromedp.FromNode(node)),
+			chromedp.Text("h2", &name, chromedp.ByQuery, chromedp.FromNode(node)),
+			chromedp.Text(".price", &price, chromedp.ByQuery, chromedp.FromNode(node)),
+		)
+	}
+	// Parsea el contenido HTML utilizando goquery
+	/*
+		doc, err := goquery.NewDocumentFromReader(response.Body())
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		var html, err2 = doc.Html()
+		if err2 != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println(html)
+
+		// Encuentra y extrae los elementos deseados utilizando selectores CSS
+		doc.Find("#app > main > div:nth-child(2) > div.col-12.col-lg-8.col-xl-9 > div:nth-child(1)").Each(func(i int, s *goquery.Selection) {
+			fmt.Println(s.Text())
 		})
-	})
-	c.Visit(fmt.Sprintf("%s/populars-boys", url))
+		/*
+			c := colly.NewCollector()
+
+			c.OnHTML("#app > main > div:nth-child(2) > div.col-12.col-lg-8.col-xl-9 > div:nth-child(1)", func(element *colly.HTMLElement) {
+				element.ForEach("div.element", func(i int, element *colly.HTMLElement) {
+					dataItentificador := element.Attr("data-identifier")
+					//fmt.Println(dataItentificador)
+					mangaPopular := models.MangaTMO{
+						Title:       element.ChildText("a > div > div > h4"),
+						MangaUrl:    element.ChildAttr("a", "href"),
+						Type:        element.ChildText("a > div > span.book-type"),
+						Demography:  element.ChildText("a > div > span.demography"),
+						Score:       element.ChildText("a > div > span.score > span"),
+						MangaImagen: getImagenManga(element.ChildText("a > div > style"), dataItentificador),
+					}
+					mangasPopulares = append(mangasPopulares, mangaPopular)
+
+				})
+			})
+
+			err := c.Visit(fmt.Sprintf("%s/populars-boys", url))
+			if err != nil {
+				fmt.Printf("Error => %s", err.Error())
+			}
+
+	*/
 	return mangasPopulares
 }
 
-//GetInfoManga obtiene la informacion de un manga
+// GetInfoManga obtiene la informacion de un manga
 func GetInfoManga(url string) models.MangaInfoTMO {
 	c := colly.NewCollector()
 	mangaInfo := models.MangaInfoTMO{}
@@ -134,7 +194,7 @@ func GetInfoManga(url string) models.MangaInfoTMO {
 	return mangaInfo
 }
 
-//getImagenManga obtiene la imagen de fondo de la lista de mangas de la pagina de inicio
+// getImagenManga obtiene la imagen de fondo de la lista de mangas de la pagina de inicio
 func getImagenManga(imagenUrl string, mangaIdentificador string) string {
 	cadenaBorrar := fmt.Sprintf(".book-thumbnail-%s::before{\n                     ", mangaIdentificador)
 
@@ -147,7 +207,7 @@ func getImagenManga(imagenUrl string, mangaIdentificador string) string {
 	return s.TrimLeft(cad3, "")
 }
 
-//getImagenListaManga obtiene la imagen de fondo de la lista de mangas de la pagina de inicio
+// getImagenListaManga obtiene la imagen de fondo de la lista de mangas de la pagina de inicio
 func getImagenListaManga(imagenUrl string, mangaIdentificador string) string {
 
 	cadenaSinBackground := s.Replace(imagenUrl, "background-image: url('", " ", -1)
@@ -166,10 +226,10 @@ func getImagenListaManga(imagenUrl string, mangaIdentificador string) string {
 
 }
 
-//https://lectormanga.com/library?title=&order_field=title&order_item=likes_count&order_dir=desc&type=&demography=seinen&webcomic=&yonkoma=&amateur=&erotic=true
-//https://lectortmo.com/library?title=&order_field=title&order_item=likes_count&order_dir=desc&type=&demography=seinen&webcomic=&yonkoma=&amateur=&erotic=true
-//https://lectortmo.com/library?order_item=likes_count&order_dir=desc&title=&_page=1&filter_by=title&type=&demography=&status=&translation_status=&webcomic=&yonkoma=&amateur=&erotic=
-//title string, orderField string, orderItem string, orderDir string, Type string, demography string, webcomic string, yonkoma string, amateur string, erotic string
+// https://lectormanga.com/library?title=&order_field=title&order_item=likes_count&order_dir=desc&type=&demography=seinen&webcomic=&yonkoma=&amateur=&erotic=true
+// https://lectortmo.com/library?title=&order_field=title&order_item=likes_count&order_dir=desc&type=&demography=seinen&webcomic=&yonkoma=&amateur=&erotic=true
+// https://lectortmo.com/library?order_item=likes_count&order_dir=desc&title=&_page=1&filter_by=title&type=&demography=&status=&translation_status=&webcomic=&yonkoma=&amateur=&erotic=
+// title string, orderField string, orderItem string, orderDir string, Type string, demography string, webcomic string, yonkoma string, amateur string, erotic string
 func BuscarMangas(order_item string, order_dir string, title string, _page string, filter_by string, Type string, demography string, status string, translation_status string, webcomic string, yonkoma string, amateur string, erotic string) []models.Library {
 	var mangas []models.Library
 	c := colly.NewCollector()
--- go.mod
+++ go.mod
@@ -4,7 +4,12 @@ go 1.15
 
 require (
 	github.com/PuerkitoBio/goquery v1.6.0
+	github.com/bozd4g/go-http-client v1.0.2 // indirect
+	github.com/chromedp/cdproto v0.0.0-20230605001715-1e95ea08ffe6 // indirect
+	github.com/chromedp/chromedp v0.9.1 // indirect
+	github.com/gobwas/ws v1.2.1 // indirect
 	github.com/gocolly/colly v1.2.0
 	github.com/gocolly/colly/v2 v2.1.0
 	github.com/gofiber/fiber/v2 v2.1.2
+	golang.org/x/sys v0.8.0 // indirect
 )
